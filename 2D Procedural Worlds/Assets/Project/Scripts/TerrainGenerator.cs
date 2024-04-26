34a38009d15373975817b86b0de7bbed6a1577dd
Dr_Pawel <Paulnatoryt@gmail.com>
2024-04-26 20:15:10 +0200

    added possiblity to change seed , also added objects spawning like rocks and bushes

--- 2D Procedural Worlds/Assets/Project/Scripts/TerrainGenerator.cs
+++ 2D Procedural Worlds/Assets/Project/Scripts/TerrainGenerator.cs
@@ -6,6 +6,7 @@ using UnityEditor;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    [Header("Map Size and Scale")]
     public int width = 256;
     public int height = 256;
     public float scale = 20f;
@@ -16,11 +17,34 @@ public class TerrainGenerator : MonoBehaviour
     public Sprite sandSprite;
     public Sprite stoneSprite;
 
+    [Header("Objects to Spawn")]
+    public List<Sprite> stoneSprites;
+    public List<Sprite> bushSprites;
+    public List<Sprite> sandBushesObjects;
+
+    [Header("Objects chances")]
+    public float stoneChance = 0.5f; 
+    public float bushChance = 0.3f;
+    public float sandBushesChance = 0.3f;
+
+    [Header("Seed Settings")]
+    [SerializeField] bool RandomGeneration = true;
+    [SerializeField] string customSeed;
+
     private int seed;
 
     private void Start()
     {
+        if(RandomGeneration == true)
+        {
         GenerateSeed();
+        }
+        else
+        {
+            seed = int.Parse(customSeed);
+        }
+
+
         Debug.Log("Current seed = " + seed);
         GenerateTerrain(); 
     }
@@ -69,11 +93,59 @@ public class TerrainGenerator : MonoBehaviour
         }
     }
 
-    private void PlaceSprite(Sprite sprite, int x, int y)
-    {
-        GameObject spriteObject = new GameObject("TerrainSprite");
-        SpriteRenderer spriteRenderer = spriteObject.AddComponent<SpriteRenderer>();
-        spriteRenderer.sprite = sprite;
-        spriteObject.transform.position = new Vector3(x, y, 0);
-    }   
-}
+    private void PlaceSprite(Sprite terrainSprite, int x, int y)
+    {       
+        GameObject terrainObject = new GameObject("TerrainSprite");
+        SpriteRenderer terrainRenderer = terrainObject.AddComponent<SpriteRenderer>();
+        terrainRenderer.sprite = terrainSprite;
+        terrainObject.transform.position = new Vector3(x, y, 0);
+
+        float randomChance = Random.Range(0f, 1f);
+
+        if (terrainSprite == dirtSprite || terrainSprite == stoneSprite)
+        {
+            if (randomChance < stoneChance)
+            {
+                Sprite randomStone = stoneSprites[Random.Range(0, stoneSprites.Count)];
+
+                GameObject stoneObject = new GameObject("StoneSprite");
+                SpriteRenderer stoneRenderer = stoneObject.AddComponent<SpriteRenderer>();
+                stoneRenderer.sprite = randomStone;
+                stoneObject.transform.position = new Vector3(x, y, 0);
+                SpriteRenderer rock = stoneObject.GetComponent<SpriteRenderer>();
+                stoneObject.AddComponent<CircleCollider2D>();
+                rock.sortingLayerName = "Props";
+            }
+        }
+        else if (terrainSprite == grassSprite)
+        {
+            if (randomChance < bushChance)
+            {
+                Sprite randomBush = bushSprites[Random.Range(0, bushSprites.Count)];
+
+                GameObject bushObject = new GameObject("BushSprite");
+                SpriteRenderer bushRenderer = bushObject.AddComponent<SpriteRenderer>();
+                bushRenderer.sprite = randomBush;
+                bushObject.transform.position = new Vector3(x, y, 0);
+                SpriteRenderer bush = bushObject.GetComponent<SpriteRenderer>();
+                bush.sortingLayerName = "Props";
+                
+            }
+        }
+        else if (terrainSprite == sandSprite)
+        {
+            if(randomChance < sandBushesChance)
+            {
+                Sprite randomBush = sandBushesObjects[Random.Range(0, sandBushesObjects.Count)];
+
+                GameObject sandBush = new GameObject("BushSprite");
+                SpriteRenderer bushRenderer = sandBush.AddComponent<SpriteRenderer>();
+                bushRenderer.sprite = randomBush;
+                sandBush.transform.position = new Vector3(x, y, 0);
+                SpriteRenderer bush = sandBush.GetComponent<SpriteRenderer>();
+                bush.sortingLayerName = "Props";
+            }
+        }
+
+    }
+}   
